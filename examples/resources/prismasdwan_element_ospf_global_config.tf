4b1624c11be71a73102dee6e0035d8b107912442
Anirudh Kushwah <akushwah@paloaltonetworks.com>
2025-03-28 00:20:50 +0530

    OPENAPI-000: element ospf global config example

--- examples/resources/prismasdwan_element_ospf_global_config.tf
+++ examples/resources/prismasdwan_element_ospf_global_config.tf
@@ -25,6 +25,30 @@
 #
 #
 
-resource "prismasdwan_element_ospf_global_config" "example" {
- // content goes here
+resource "prismasdwan_resource_locator" "element_site" {
+  resource_type           = "prismasdwan_site"
+  resource_property       = "name"
+  resource_property_value = "element_shell_site"
+}
+
+resource "prismasdwan_resource_locator" "element_shell" {
+  resource_type           = "prismasdwan_element"
+  resource_property       = "name"
+  resource_property_value = "manual_device_shell"
+}
+
+resource "prismasdwan_element_ospf_global_config" "tf_element_ospf_global_config" {
+  x_parameters = {
+    site_id    = prismasdwan_resource_locator.element_site.result
+    element_id = prismasdwan_resource_locator.element_shell.result
+  }
+  cost                   = 100
+  dead_interval          = 40
+  hello_interval         = 10
+  md5_key_id             = 12
+  md5_secret             = base64encode("garbage_pwd")
+  prefix_adv_type_to_lan = "unaggregated"
+  retransmit_interval    = 5
+  router_id              = ""
+  transmit_delay         = 1
 }
