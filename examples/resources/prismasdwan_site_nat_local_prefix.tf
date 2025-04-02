fc7fd0599db245ade2e41d20ce5044a48e90d8f8
Vishwas PG <vpg@paloaltonetworks.com>
2025-04-02 17:35:30 +0530

    OPENAPI-000: site nat localprefix example

--- examples/resources/prismasdwan_site_nat_local_prefix.tf
+++ examples/resources/prismasdwan_site_nat_local_prefix.tf
@@ -25,6 +25,12 @@
 #
 #
 
-resource "prismasdwan_site_nat_local_prefix" "example" {
- // content goes here
+resource "prismasdwan_site_nat_local_prefix" "test_site_nat_local_prefix_1" {
+  x_parameters = {
+    site_id = prismasdwan_site.site_1.id
+  }
+  prefix_id = prismasdwan_nat_local_prefix.test_nat_local_prefix_01.id
+  ipv4_prefixes = [
+    "10.11.12.13/32"
+  ]
 }
