d0d711e95679e11ea2ea4446b97343688542b924
Vishwas PG <vpg@paloaltonetworks.com>
2025-04-02 17:41:21 +0530

    OPENAPI-000: site security policy localprefix example

--- examples/resources/prismasdwan_site_security_policy_local_prefix.tf
+++ examples/resources/prismasdwan_site_security_policy_local_prefix.tf
@@ -25,6 +25,12 @@
 #
 #
 
-resource "prismasdwan_site_security_policy_local_prefix" "example" {
- // content goes here
+resource "prismasdwan_site_security_policy_local_prefix" "example_security_policy_local_prefix" {
+  x_parameters = {
+    site_id = prismasdwan_site.site_1.id
+  }
+  prefix_id = prismasdwan_security_policy_local_prefix.test_security_local_prefix.id
+  ipv4_prefixes = [
+    "10.11.12.13/32"
+  ]
 }
