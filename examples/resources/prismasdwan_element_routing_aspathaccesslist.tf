ca3916a3eccc3ab40378d139f395d90c6849bdb4
Vishwas PG <vpg@paloaltonetworks.com>
2025-04-02 16:50:29 +0530

    OPENAPI-000: element routing aspathaccesslist example

--- examples/resources/prismasdwan_element_routing_aspathaccesslist.tf
+++ examples/resources/prismasdwan_element_routing_aspathaccesslist.tf
@@ -25,6 +25,30 @@
 #
 #
 
-resource "prismasdwan_element_routing_aspathaccesslist" "example" {
- // content goes here
+resource "prismasdwan_element_routing_aspathaccesslist" "example_routing_aspathaccesslist" {
+ x_parameters = {
+    site_id = prismasdwan_site.site_1.id
+    element_id = prismasdwan_element.element_1.id
+  }
+  name = "example_routing_aspathaccesslist"
+  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  tags = ["lorem", "ipsum"]
+  auto_generated = false
+  as_path_regex_list = [
+    {
+      order = 1
+      permit = true
+      as_path_regex = "^6602$"
+    },
+    {
+      order = 11
+      permit = true
+      as_path_regex = "^765"
+    },
+    {
+      order = 21
+      permit = true
+      as_path_regex = "^ab123"
+    }
+  ]
 }
