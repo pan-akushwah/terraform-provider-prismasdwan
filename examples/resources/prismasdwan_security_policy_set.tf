3729fad359e2ed7f4b16ca769693a0ab77ee17eb
Anirudh Kushwah <akushwah@paloaltonetworks.com>
2026-01-16 22:24:47 +0530

    OPENAPI-000: example update for security policy set

--- examples/resources/prismasdwan_security_policy_set.tf
+++ examples/resources/prismasdwan_security_policy_set.tf
@@ -27,21 +27,111 @@
 
 resource "prismasdwan_security_policy_set" "production_policy_set" {
   name                  = "production_security_policy_set"
-  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  description           = "Managed by Prisma SDWAN Terraform IaaC Provider"
   tags                  = ["production", "high_priority"]
   defaultrule_policyset = false
 }
 
 resource "prismasdwan_security_policy_set" "development_policy_set" {
   name                  = "development_security_policy_set"
-  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  description           = "Managed by Prisma SDWAN Terraform IaaC Provider"
   tags                  = ["development", "testing"]
   defaultrule_policyset = false
 }
 
 resource "prismasdwan_security_policy_set" "compliance_policy_set" {
   name                  = "compliance_security_policy_set"
-  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  description           = "Managed by Prisma SDWAN Terraform IaaC Provider"
   tags                  = ["compliance", "audit"]
   defaultrule_policyset = true
+}
+
+#
+# Below Example will create a set named `rule_ordered_policy_set`
+# with three rules in Order [rule_1, rule_2, rule_3]
+# NOTE THE `depends_on` between rules
+# 
+resource "prismasdwan_security_policy_set" "rule_ordered_policy_set" {
+  defaultrule_policyset = false
+  name                  = "rule_ordered_policy_set"
+}
+
+
+#
+# rule_1 will be created after rule_ordered_policy_set is created
+# this is due to reference of rule_ordered_policy_set in x_parameters
+#
+resource "prismasdwan_security_policy_rule" "rule_1" {
+  x_parameters = {
+    "policy_set_id" = prismasdwan_security_policy_set.rule_ordered_policy_set.id
+  }
+
+  name        = "rule_1"
+  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  enabled     = true
+  action      = "allow"
+
+  # List attributes
+  app_def_ids            = []
+  source_zone_ids        = []
+  source_prefix_ids      = []
+  destination_prefix_ids = []
+  destination_zone_ids   = []
+  services               = []
+
+  # Nullable attributes
+  tags = ["tag1", "tag2"]
+}
+
+
+#
+# rule_2 will be created after rule_1 is created
+# this will cause rule_2 to be set after rule_1
+#
+resource "prismasdwan_security_policy_rule" "rule_2" {
+  x_parameters = {
+    "policy_set_id" = prismasdwan_security_policy_set.rule_ordered_policy_set.id
+  }
+  depends_on  = [prismasdwan_security_policy_rule.rule_1]
+  name        = "rule_2"
+  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  enabled     = true
+  action      = "allow"
+
+  # List attributes
+  app_def_ids            = []
+  source_zone_ids        = []
+  source_prefix_ids      = []
+  destination_prefix_ids = []
+  destination_zone_ids   = []
+  services               = []
+
+  # Nullable attributes
+  tags = ["tag1", "tag2"]
+}
+
+#
+# rule_3 will be created after rule_2 is created
+# this will cause rule_3 to be set after rule_2
+#
+resource "prismasdwan_security_policy_rule" "rule_3" {
+  x_parameters = {
+    "policy_set_id" = prismasdwan_security_policy_set.rule_ordered_policy_set.id
+  }
+  depends_on  = [prismasdwan_security_policy_rule.rule_2]
+  name        = "rule_3"
+  description = "Managed by Prisma SDWAN Terraform IaaC Provider"
+  enabled     = true
+  action      = "allow"
+
+  # List attributes
+  app_def_ids            = []
+  source_zone_ids        = []
+  source_prefix_ids      = []
+  destination_prefix_ids = []
+  destination_zone_ids   = []
+  services               = []
+
+  # Nullable attributes
+  tags = ["tag1", "tag2"]
 }
